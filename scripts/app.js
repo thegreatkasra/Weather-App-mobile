364c932be16f40b18ebf1304ea1899704096a191
kasra torabi <Kasra.torabiii@gmail.com>
2024-09-01 09:13:09 +0330

    first push

---
+++ scripts/app.js
@@ -0,0 +1,60 @@
+const apiKey = '9aa5cafb8fbf68fd133c0f3929abb9e6';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404){
+        document.querySelector('.error').style.display = "block";
+    }
+
+    const data = await response.json();
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
+    document.getElementById('humidity').innerHTML = data.main.humidity + ' %';
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
+
+    if (data.weather[0].main == "Clear"){
+        weatherIcon.src = "../images/clear.png"; 
+    }
+    else if (data.weather[0].main == 'Clouds'){
+        weatherIcon.src = "../images/clouds.png"; 
+    }
+    else if (data.weather[0].main == 'Rain'){
+        weatherIcon.src = "../images/rain.png"; 
+    }
+    else if (data.weather[0].main == 'Drizzle'){
+        weatherIcon.src = "../images/drizzle.png"; 
+    }
+    else if (data.weather[0].main == 'Mist'){
+        weatherIcon.src = "../images/mist.png"; 
+    }
+}
+
+searchButton.addEventListener('click', () => {
+    const city = searchBox.value;
+    if (city) {
+        checkWeather(city);
+    } else {
+        alert("Please enter a city name");
+    }
+});
+
+searchBox.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const city = searchBox.value;
+        if (city) {
+            checkWeather(city);
+        } else {
+            alert("Please enter a city name");
+        }
+    }
+});
+
+
+
